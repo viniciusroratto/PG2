f2dd2e438f6935388861eaf7694b6bc5333d3932
viniciusroratto <vini.jornal@gmail.com>
2021-10-12 18:11:24 -0300

    first commit

---
+++ Cliente.java
@@ -0,0 +1,8 @@
+package webservice;
+
+public class Cliente {
+     
+    public static void main(String[] args) {
+         
+    }
+}
---
+++ Servidor.java
@@ -0,0 +1,8 @@
+package webservice;
+   
+public class Servidor {
+
+    public static void main(String[] args) {
+
+    }
+}
